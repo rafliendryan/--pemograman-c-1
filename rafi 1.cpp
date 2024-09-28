5ac85164e2c9aadc38cb547b37e88061df1e4b11
rafliendryan <rafliendrian@gmail.com>
2024-09-28 08:27:28 +0700

    Add files via upload

---
+++ rafi 1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+    
+    char nim[] = "2441077"; 
+    char nama[] = "RAFLI ENDRYAN"; 
+    char jurusan[] = "Teknik Informatika"; 
+
+    printf("NIM: %s\n", nim);
+    printf("NAMA: %s\n", nama);
+    printf("JURUSAN: %s\n", jurusan);
+
+    return 0;
+}
+
